e7d07e574c49739b84912233859c8dc5c69c4270
shamolmojumder <shamolmojumder14@gmail.com>
2020-11-08 01:06:32 +0600

    getting started with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import './App.css';
+import News from './components/News/News';
 
 function App() {
   return (
     <div>
     <h2>React Button</h2>
-    <Button variant="primary">Primary</Button>
+    <News></News>
   </div>
   );
 }
