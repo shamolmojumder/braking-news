4d320ae62f40b094a145a80ad96530cc1d338a71
shamolmojumder <shamolmojumder14@gmail.com>
2020-11-08 01:19:04 +0600

    Jumbotron added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import './App.css';
+import Header from './components/Header/Header';
 import News from './components/News/News';
 
 function App() {
   return (
     <div>
     <h2>React Button</h2>
+    <Header></Header>
     <News></News>
   </div>
   );
